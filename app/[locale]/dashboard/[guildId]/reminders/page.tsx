e6b35359934bd08b606c915134d6f269820c1fa3
Joel <40491616+Joelsuperstar@users.noreply.github.com>
2026-01-11 14:38:22 +0100

    chore: changed some strings with reminders

--- app/[locale]/dashboard/[guildId]/reminders/page.tsx
+++ app/[locale]/dashboard/[guildId]/reminders/page.tsx
@@ -963,9 +963,6 @@ export default function RemindersPage() {
                       onChange={(e) => updateDialogField("time", e.target.value)}
                       className="flex-1"
                     />
-                    <span className="text-sm text-muted-foreground whitespace-nowrap">
-                      {t('card.timeUnit')}
-                    </span>
                   </div>
                 </div>
 
