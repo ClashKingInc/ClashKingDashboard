306d3f2417ae184a29fa4bc1237377108230e005
Joel <40491616+Joelsuperstar@users.noreply.github.com>
2026-01-12 13:03:05 +0100

    chore: change the assets links to use the base url

--- lib/theme.ts
+++ lib/theme.ts
@@ -42,92 +42,94 @@ export const clashKingColors = {
  * ClashKing logos and assets URLs
  * CDN: https://assets.clashk.ing
  */
+const baseUrl = 'https://assets.clashk.ing';
+
 export const clashKingAssets = {
   // CDN base URL
-  baseUrl: 'https://assets.clashk.ing',
+  baseUrl,
 
   logos: {
     // OFFICIAL LOGOS (crown + arrow) - Preferred
     // Use these logos for the dashboard
-    darkBg: 'https://assets.clashk.ing/logos/crown-arrow-dark-bg/ClashKing-1.svg',
-    darkBgPng: 'https://assets.clashk.ing/logos/crown-arrow-dark-bg/ClashKing-1.png',
-    whiteBg: 'https://assets.clashk.ing/logos/crown-arrow-white-bg/ClashKing-2.svg',
-    whiteBgPng: 'https://assets.clashk.ing/logos/crown-arrow-white-bg/ClashKing-2.png',
+    darkBg: `${baseUrl}/logos/crown-arrow-dark-bg/ClashKing-1.svg`,
+    darkBgPng: `${baseUrl}/logos/crown-arrow-dark-bg/ClashKing-1.png`,
+    whiteBg: `${baseUrl}/logos/crown-arrow-white-bg/ClashKing-2.svg`,
+    whiteBgPng: `${baseUrl}/logos/crown-arrow-white-bg/ClashKing-2.png`,
 
     // LOGOS WITH "ClashKing" TEXT
-    withTextDark: 'https://assets.clashk.ing/logos/crown-text-dark-bg/ClashKing-with-text-3.svg',
-    withTextDarkPng: 'https://assets.clashk.ing/logos/crown-text-dark-bg/KTYEp1081709208tL7z4HA7BiA2MucfE08fgsIuKdtnuf37cwqzbdp9qpnj2j.png',
-    withTextWhite: 'https://assets.clashk.ing/logos/crown-text-white-bg/ClashKing-with-text-2.svg',
-    withTextWhitePng: 'https://assets.clashk.ing/logos/crown-text-white-bg/BqlEp974170917vB1qK0zunfANJCGi0W031dTksEq7KQ9LoXWMFk0u77unHJa.png',
+    withTextDark: `${baseUrl}/logos/crown-text-dark-bg/ClashKing-with-text-3.svg`,
+    withTextDarkPng: `${baseUrl}/logos/crown-text-dark-bg/KTYEp1081709208tL7z4HA7BiA2MucfE08fgsIuKdtnuf37cwqzbdp9qpnj2j.png`,
+    withTextWhite: `${baseUrl}/logos/crown-text-white-bg/ClashKing-with-text-2.svg`,
+    withTextWhitePng: `${baseUrl}/logos/crown-text-white-bg/BqlEp974170917vB1qK0zunfANJCGi0W031dTksEq7KQ9LoXWMFk0u77unHJa.png`,
 
     // TEXT LOGOS (for loading screens)
-    textDarkBg: 'https://assets.clashk.ing/logos/crown-arrow-dark-bg/CK-text-dark-bg.png',
-    textWhiteBg: 'https://assets.clashk.ing/logos/crown-arrow-white-bg/CK-text-white-bg.png',
+    textDarkBg: `${baseUrl}/logos/crown-arrow-dark-bg/CK-text-dark-bg.png`,
+    textWhiteBg: `${baseUrl}/logos/crown-arrow-white-bg/CK-text-white-bg.png`,
 
     // ICONS (crown only)
-    crownRed: 'https://assets.clashk.ing/logos/crown-red/ClashKing-crown.svg',
-    iconBlack: 'https://assets.clashk.ing/logos/crown-black-arrow-white-bg/ClashKing-icon-black.svg',
-    iconBlackPng: 'https://assets.clashk.ing/logos/crown-black-arrow-white-bg/Rl3Ep2541709222E0oUYaLObTdAOBkQ6MiAb16i3iq7ROnNlky4kqZQPS8Ku6.png',
-    iconWhite: 'https://assets.clashk.ing/logos/crown-white-arrow-dark-bg/ClashKing-icon-white.svg',
-    iconWhitePng: 'https://assets.clashk.ing/logos/crown-white-arrow-dark-bg/FjTEp708170921wcQnjjhKgCZNfLjaMO7uQy3lFnLiwcsjtNluf1CyZ3e7QtL.png',
-    crownBlack: 'https://assets.clashk.ing/logos/crown-black/crown-black.svg',
-    crownWhite: 'https://assets.clashk.ing/logos/crown-white/crown-white.svg',
+    crownRed: `${baseUrl}/logos/crown-red/ClashKing-crown.svg`,
+    iconBlack: `${baseUrl}/logos/crown-black-arrow-white-bg/ClashKing-icon-black.svg`,
+    iconBlackPng: `${baseUrl}/logos/crown-black-arrow-white-bg/Rl3Ep2541709222E0oUYaLObTdAOBkQ6MiAb16i3iq7ROnNlky4kqZQPS8Ku6.png`,
+    iconWhite: `${baseUrl}/logos/crown-white-arrow-dark-bg/ClashKing-icon-white.svg`,
+    iconWhitePng: `${baseUrl}/logos/crown-white-arrow-dark-bg/FjTEp708170921wcQnjjhKgCZNfLjaMO7uQy3lFnLiwcsjtNluf1CyZ3e7QtL.png`,
+    crownBlack: `${baseUrl}/logos/crown-black/crown-black.svg`,
+    crownWhite: `${baseUrl}/logos/crown-white/crown-white.svg`,
 
     // BOT/APP LOGOS
-    botApp: 'https://assets.clashk.ing/logos/bot-app-logo/bot-app-logo.png',
-    christmas: 'https://assets.clashk.ing/logos/bot-app-logo/christmas-logo.gif',
+    botApp: `${baseUrl}/logos/bot-app-logo/bot-app-logo.png`,
+    christmas: `${baseUrl}/logos/bot-app-logo/christmas-logo.gif`,
   },
 
   // Available game icons
   icons: {
     // Clash Royale / Discord (Icon_DC_*)
     dc: {
-      cwl: 'https://assets.clashk.ing/icons/Icon_DC_CWL.png',
-      war: 'https://assets.clashk.ing/icons/Icon_DC_War.png',
-      hitrate: 'https://assets.clashk.ing/icons/Icon_DC_Hitrate.png',
-      cross: 'https://assets.clashk.ing/icons/Icon_DC_Cross.png',
-      tick: 'https://assets.clashk.ing/icons/Icon_DC_Tick.png',
-      arrowLeft: 'https://assets.clashk.ing/icons/Icon_DC_ArrowLeft.png',
-      arrowRight: 'https://assets.clashk.ing/icons/Icon_DC_ArrowRight.png',
+      cwl: `${baseUrl}/icons/Icon_DC_CWL.png`,
+      war: `${baseUrl}/icons/Icon_DC_War.png`,
+      hitrate: `${baseUrl}/icons/Icon_DC_Hitrate.png`,
+      cross: `${baseUrl}/icons/Icon_DC_Cross.png`,
+      tick: `${baseUrl}/icons/Icon_DC_Tick.png`,
+      arrowLeft: `${baseUrl}/icons/Icon_DC_ArrowLeft.png`,
+      arrowRight: `${baseUrl}/icons/Icon_DC_ArrowRight.png`,
     },
     // Clash of Clans - Home Village (Icon_HV_*)
     hv: {
-      attack: 'https://assets.clashk.ing/icons/Icon_HV_Attack.png',
-      shield: 'https://assets.clashk.ing/icons/Icon_HV_Shield.png',
-      trophy: 'https://assets.clashk.ing/icons/Icon_HV_Trophy.png',
-      legendLeague: 'https://assets.clashk.ing/icons/Icon_HV_League_Legend_3.png',
-      clanWar: 'https://assets.clashk.ing/icons/Icon_HV_Clan_War.png',
-      goldPass: 'https://assets.clashk.ing/icons/Icon_HV_Gold_Pass.png',
-      raidAttack: 'https://assets.clashk.ing/icons/Icon_HV_Raid_Attack.png',
-      capitalGold: 'https://assets.clashk.ing/icons/Icon_HV_Capital_Gold.png',
-      capitalTrophy: 'https://assets.clashk.ing/icons/Icon_HV_Capital_Trophy.png',
+      attack: `${baseUrl}/icons/Icon_HV_Attack.png`,
+      shield: `${baseUrl}/icons/Icon_HV_Shield.png`,
+      trophy: `${baseUrl}/icons/Icon_HV_Trophy.png`,
+      legendLeague: `${baseUrl}/icons/Icon_HV_League_Legend_3.png`,
+      clanWar: `${baseUrl}/icons/Icon_HV_Clan_War.png`,
+      goldPass: `${baseUrl}/icons/Icon_HV_Gold_Pass.png`,
+      raidAttack: `${baseUrl}/icons/Icon_HV_Raid_Attack.png`,
+      capitalGold: `${baseUrl}/icons/Icon_HV_Capital_Gold.png`,
+      capitalTrophy: `${baseUrl}/icons/Icon_HV_Capital_Trophy.png`,
     },
     // Builder Base (Icon_BB_*)
     bb: {
-      starEmpty: 'https://assets.clashk.ing/icons/Icon_BB_Star_Empty.png',
-      starFilled: 'https://assets.clashk.ing/icons/Icon_BB_Star_Filled.png',
+      starEmpty: `${baseUrl}/icons/Icon_BB_Star_Empty.png`,
+      starFilled: `${baseUrl}/icons/Icon_BB_Star_Filled.png`,
     },
     // Generic icons
     generic: {
-      startFlag: 'https://assets.clashk.ing/icons/Icon_HV_Start_Flag.png',
-      sword: 'https://assets.clashk.ing/icons/Icon_HV_Sword.png',
-      podium: 'https://assets.clashk.ing/icons/Icon_HV_Podium.png',
-      shieldArrow: 'https://assets.clashk.ing/icons/Icon_HV_Shield_Arrow.png',
-      unknownPerson: 'https://assets.clashk.ing/icons/Unknown_person.jpg',
+      startFlag: `${baseUrl}/icons/Icon_HV_Start_Flag.png`,
+      sword: `${baseUrl}/icons/Icon_HV_Sword.png`,
+      podium: `${baseUrl}/icons/Icon_HV_Podium.png`,
+      shieldArrow: `${baseUrl}/icons/Icon_HV_Shield_Arrow.png`,
+      unknownPerson: `${baseUrl}/icons/Unknown_person.jpg`,
     },
   },
 
   // Paths to asset folders
   paths: {
-    logos: 'https://assets.clashk.ing/logos',
-    icons: 'https://assets.clashk.ing/icons',
-    flags: 'https://assets.clashk.ing/country-flags',
-    stickers: 'https://assets.clashk.ing/stickers',
-    defaultPics: 'https://assets.clashk.ing/default-pics',
-    fonts: 'https://assets.clashk.ing/fonts',
-    homeBase: 'https://assets.clashk.ing/home-base',
-    builderBase: 'https://assets.clashk.ing/builder-base',
-    capitalBase: 'https://assets.clashk.ing/capital-base',
+    logos: `${baseUrl}/logos`,
+    icons: `${baseUrl}/icons`,
+    flags: `${baseUrl}/country-flags`,
+    stickers: `${baseUrl}/stickers`,
+    defaultPics: `${baseUrl}/default-pics`,
+    fonts: `${baseUrl}/fonts`,
+    homeBase: `${baseUrl}/home-base`,
+    builderBase: `${baseUrl}/builder-base`,
+    capitalBase: `${baseUrl}/capital-base`,
   },
 } as const;
 
